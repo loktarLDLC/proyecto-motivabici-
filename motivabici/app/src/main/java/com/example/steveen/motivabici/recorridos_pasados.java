41efb133bbf2b74b39b0aa7da17d62edfcd4882d
loktarLDLC <metalsteveen@gmail.com>
2016-09-03 12:11:35 -0500

    recorridos pasados clase y xml

---
+++ motivabici/app/src/main/java/com/example/steveen/motivabici/recorridos_pasados.java
@@ -0,0 +1,19 @@
+package com.example.steveen.motivabici;
+
+/**
+ * Created by steveen on 03/09/2016.
+ */
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class recorridos_pasados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recorridos_pasados);
+
+    }
+}
