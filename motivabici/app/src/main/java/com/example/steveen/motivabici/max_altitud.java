f668e90830af64d1f550ccfa7feeb50086e33522
loktarLDLC <metalsteveen@gmail.com>
2016-09-03 10:22:36 -0500

    alt max clase y xml

---
+++ motivabici/app/src/main/java/com/example/steveen/motivabici/max_altitud.java
@@ -0,0 +1,17 @@
+package com.example.steveen.motivabici;
+
+/**
+ * Created by steveen on 03/09/2016.
+ */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class max_altitud extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_max_altitud);
+    }
+}
+
