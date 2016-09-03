69a1796ba69a46957e68d3ee06fe8ca6b27e62c6
loktarLDLC <metalsteveen@gmail.com>
2016-09-03 10:44:53 -0500

    calorias quemadas clase y xml

---
+++ motivabici/app/src/main/java/com/example/steveen/motivabici/calorias_quemadas.java
@@ -0,0 +1,16 @@
+package com.example.steveen.motivabici;
+
+/**
+ * Created by steveen on 03/09/2016.
+ */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class calorias_quemadas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calorias_quemadas);
+    }
+}
